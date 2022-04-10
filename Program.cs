65a85eb760fdcb8915e1f2e3bf579f5362bc0baf
Michael Mathu <mathumichael17@gmail.com>
2022-04-10 10:55:05 +0300

    added constructors

--- Program.cs
+++ Program.cs
@@ -8,7 +8,20 @@
         private int pages;
         private int wordCount;
 
-       
+        //Constructor. Allows the user of this class to instanciate the instant variables and give them a starting state/initial values
+        public Book(string title, string author)
+        {
+            //class level titile = method level title;
+            this.title = title;
+            this.author = author;
+        }
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
 
     }
 }
