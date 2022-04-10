d5eaf33494a2c27d073bad31d5bd606e14f6b084
Michael Mathu <mathumichael17@gmail.com>
2022-04-10 10:54:36 +0300

    added instant variables

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CreatingClasses
+{
+    class Book
+    {
+        //Instanct Variables
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+       
+
+    }
+}
